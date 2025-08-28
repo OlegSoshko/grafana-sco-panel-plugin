7ceaba6ff9f47dd027650703c7ac5209b99824c5
Oleg Soshko <soshko.oa@lime-shop.com>
2025-08-28 14:34:26 +0300

    добавил подсветку контейнера если нет данных и взял цвета из темы

--- src/utils/getColorTextByValue.ts
+++ src/utils/getColorTextByValue.ts
@@ -1,15 +1,20 @@
-export const getColorTextByValue = (value: number | undefined | null): string => {
-  if (!value) return '#ff9900';
+import { GrafanaTheme2 } from "@grafana/data";
+
+const defaultColor = '#ff9900'
+
+export const getColorTextByValue = (theme: GrafanaTheme2, value: number | undefined | null): string => {
+  if (!theme) return defaultColor;
+  if (!value) return theme.colors.error.text;
 
   if (value >= 0 && value < 5) {
-    return '#5794f2';
+    return theme.colors.primary.text;
   } else if (value >= 0 && value < 50) {
-    return '#73BF69';
+    return theme.colors.success.text;
   } else if (value >= 50 && value < 80) {
-    return '#EAB839';
+    return theme.colors.warning.text;
   } else if (value >= 80 && value <= 100) {
-    return '#E24D42';
+    return theme.colors.error.text;
   }
 
-  return '#ff9900';
+  return defaultColor;
 };
